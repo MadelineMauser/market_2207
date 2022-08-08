a7ecea7f0983e445658a82e8328e785825cc8bd4
Madeline Mauser <madeline.mauser@gmail.com>
2022-08-08 09:58:57 -0600

    Fix tests for #stock

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Vendor do
   before :all do
     @item1 = Item.new({name: 'Peach', price: "$0.75"})
     @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+  before :each do
     @vendor = Vendor.new("Rocky Mountain Fresh")
   end
   describe 'Vendor instantiation' do
@@ -25,14 +27,12 @@ RSpec.describe Vendor do
   end
   describe '#stock' do
     it 'can stock a given quantity of a given item' do
-      expect(@vendor.inventory).to eq({@item1 => 0})
       @vendor.stock(@item1, 30)
       expect(@vendor.inventory).to eq({@item1 => 30})
       @vendor.stock(@item1, 25)
       expect(@vendor.inventory).to eq({@item1 => 55})
     end
     it 'can stock multiple items' do
-      expect(@vendor.inventory).to eq({@item1 => 0})
       @vendor.stock(@item1, 30)
       @vendor.stock(@item2, 12)
       expect(@vendor.inventory).to eq({@item1 => 30, @item2 => 12})
